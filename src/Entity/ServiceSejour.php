4a7e4fe3fd653c35446849c08c233cc985d7f67c
flori <florimond2002@gmail.com>
2021-11-03 21:06:44 +0100

    exposition des entités via l'api

--- src/Entity/ServiceSejour.php
+++ src/Entity/ServiceSejour.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ServiceSejourRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=ServiceSejourRepository::class)
+ * @ApiResource()
  */
 class ServiceSejour
 {
