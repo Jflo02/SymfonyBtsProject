99d0e934f0bdc15eb73240edbe473075f0008f48
Piett-rm <rmpetit24@gmail.com>
2021-11-18 12:09:42 +0100

    ajout Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,7 +7,10 @@ use App\Entity\Infirmier;
 use App\Entity\Lit;
 use App\Entity\Patient;
 use App\Entity\Sejour;
+use App\Entity\ServiceSejour;
 use App\Entity\User;
+use App\Repository\SejourRepository;
+use App\Repository\ServiceRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -23,24 +26,32 @@ class AppFixtures extends Fixture
      */
     private $passwordHasher;
 
-    public function __construct(UserPasswordHasherInterface $passwordHasher){
-        $this->passwordHasher= $passwordHasher;
+    private $serviceRepository;
+
+    private $sejourRepository;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher, ServiceRepository $serviceRepository, SejourRepository $sejourRepository)
+    {
+        $this->serviceRepository = $serviceRepository;
+        $this->sejourRepository = $sejourRepository;
+        $this->passwordHasher = $passwordHasher;
     }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
 
         $chambreTab = [];
-        $litTab= [];
-        $patientTab= [];
+        $litTab = [];
+        $patientTab = [];
 
-        for ($c= 0; $c < 15; $c++){
+        for ($c = 0; $c < 15; $c++) {
             $chambre = new Chambre();
-            array_push($chambreTab,$chambre);
+            array_push($chambreTab, $chambre);
             $manager->persist($chambre);
         }
 
-        for($l=0; $l<30; $l++) {
+        for ($l = 0; $l < 30; $l++) {
             $lit = new Lit();
             array_push($litTab, $lit);
             $lit->setChambre($faker->randomElement($chambreTab));
@@ -48,26 +59,26 @@ class AppFixtures extends Fixture
         }
 
 
-        for ($p = 0; $p < 30; $p++){
+        for ($p = 0; $p < 30; $p++) {
             $patient = new Patient();
-            array_push($patientTab,$patient);
+            array_push($patientTab, $patient);
             $patient->setPrenom($faker->firstName)
-                    ->setNom($faker->lastName)
-                    ->setAge($faker->numberBetween(0,100));
+                ->setNom($faker->lastName)
+                ->setAge($faker->numberBetween(0, 100));
             $manager->persist($patient);
 
-            for ($s=0 ; $s < mt_rand(1,5); $s++){
+            for ($s = 0; $s < mt_rand(1, 5); $s++) {
                 $sejour = new Sejour();
-                $sejour->setDateEntree($faker->dateTimeBetween($startDate='-2 years',$endDate='-1 years'))
-                       ->setDateSortie($faker->dateTimeBetween($startDate='-1 years',$endDate='now'))
-                       ->setLit($faker->randomElement($litTab))
-                       ->setPatient($patient);
+                $sejour->setDateEntree($faker->dateTimeBetween($startDate = '-2 years', $endDate = '-1 years'))
+                    ->setDateSortie($faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now'))
+                    ->setLit($faker->randomElement($litTab))
+                    ->setPatient($patient);
                 $manager->persist($sejour);
 
             }
         }
 
-        for ($u = 0; $u<3; $u++){
+        for ($u = 0; $u < 3; $u++) {
             $user = new User();
 
             $hash = $this->passwordHasher->hashPassword($user, "Epsi.123");
@@ -77,8 +88,31 @@ class AppFixtures extends Fixture
             $manager->persist($user);
         }
 
+        $serviceTab = $this->serviceRepository->findAll();
+
+        $sejourTab = $this->sejourRepository->findAll();
+
+        for ($i = 0; $i < 20; $i++) {
+            $infirmier = new Infirmier();
+            $infirmier->setPrenom($faker->firstName)
+                ->setNom($faker->lastName)
+                ->setAge($faker->numberBetween(19, 65))
+                ->setMotDePasse('Azerty')
+                ->setService($faker->randomElement($serviceTab));
+
+            $manager->persist($infirmier);
+        }
 
 
+        for ($ss = 0; $ss < 70; $ss++) {
+            $serviceSejour = new ServiceSejour();
+            $serviceSejour->setService($faker->randomElement($serviceTab))
+                ->setSejour($faker->randomElement($sejourTab))
+                ->setDateEntree($faker->dateTimeBetween($startDate = '-2 years', $endDate = '-1 years'))
+                ->setDateSortie($faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now'));
+            $manager->persist($serviceSejour);
+
+        }
 
 
         $manager->flush();
