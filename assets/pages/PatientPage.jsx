e772bebdadf91b0d86552382db0bdb8c211f938e
Piett-rm <rmpetit24@gmail.com>
2021-12-03 22:10:17 +0100

    ajout d'un patient ok + modif patient la requete ne fonctionne pas

--- assets/pages/PatientPage.jsx
+++ assets/pages/PatientPage.jsx
@@ -1,49 +1,100 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Field from "./../components/forms/Field";
 import axios from "axios";
-import serverAddress from '../consts/ServerAddress';
-
+import serverAddress from "../consts/ServerAddress";
 
 const PatientPage = (props) => {
+  const { id = "new" } = props.match.params;
+
   const [patient, setPatient] = useState({
-    "lastName": "Aurélien",
-    "firstName": "",
-    "age": 0,
-    "sejours": []
+    lastName: "",
+    firstName: "",
+    age: 0,
+    sejours: [],
   });
 
   const [errors, setErrors] = useState({
-    lastName: "Le nom est obligatoire",
-    firstName: "Le prennom est obligatoire",
-    age: "L'age est obligatoire",
+    lastName: "",
+    firstName: "",
+    age: "",
   });
+
+  const [editing, setEditing] = useState(false);
+
+  const fetchPatient = async (id) => {
+    try {
+      const data = await axios
+        .get(serverAddress + "/api/patients/" + id)
+        .then((response) => response.data);
+      console.log(data);
+      const { nom, prenom, age, sejours } = data;
+
+      setPatient({
+        lastName: nom,
+        firstName: prenom,
+        age: age,
+        sejours: sejours,
+      });
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    if (id != "new") {
+      setEditing(true);
+      fetchPatient(id);
+    }
+    // a chaque fois que l'id a changé
+  }, [id]);
 
   const handleChange = ({ currentTarget }) => {
     const { name, value } = currentTarget;
     setPatient({ ...patient, [name]: value });
   };
 
-  const handleSubmit =  async event => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    try{
-        console.log(patient)
-        const response = await axios.post(serverAddress+'/api/patients', {
-            age:Number(patient.age),
-            nom:patient.lastName,
-            prenom:patient.firstName,
-            sejours:patient.sejours
-        })
-        console.log(response.data);
-    } catch(error) {
-        console.log(error.response)
+    try {
+      if (editing) {
+        console.log(patient);
+        const reponse = await axios.put(serverAddress + "/api/patients/" + id, {
+          id: id,
+          age: Number(patient.age),
+          nom: patient.lastName,
+          prenom: patient.firstName,
+          sejours: patient.sejours,
+        });
+
+        console.log(reponse);
+        console.log(reponse.data);
+      } else {
+        setErrors({});
+        const response = await axios.post(serverAddress + "/api/patients", {
+          age: Number(patient.age),
+          nom: patient.lastName,
+          prenom: patient.firstName,
+          sejours: patient.sejours,
+        });
+      }
+    } catch (error) {
+      if (error.response.data.violations) {
+        const apiErrors = {};
+      }
+      error.response.data.violations.forEach((violation) => {
+        apiErrors[violations.propertyPath] = violation.message;
+      });
+      setErrors(apiErrors);
     }
   };
 
   return (
     <>
-      <h1>Création d'un patient</h1>
+      {(!editing && <h1>Création d'un patient</h1>) || (
+        <h1>Modification d'un patient</h1>
+      )}
 
       <form onSubmit={handleSubmit}>
         <Field
